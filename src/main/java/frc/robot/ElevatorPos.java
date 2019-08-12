6da25eb4f30403b9d57bb5282e33ca4c35a8de9f
Bill Xiang <bill28.eaton@gmail.com>
2019-08-11 17:12:04 -0700

    added clarifying comments to the ElevatorPos enum

--- src/main/java/frc/robot/ElevatorPos.java
+++ src/main/java/frc/robot/ElevatorPos.java
@@ -5,11 +5,15 @@ import frc.robot.Constants;
 public enum ElevatorPos {
     ZERO(0), 
     ZERO_ABOVE(Constants.SAFE_ELEVATOR_GAP),
-    CARGO_LOW_BELOW(Constants.BOX-Constants.SAFE_ELEVATOR_GAP), 
     CARGO_LOW(Constants.BOX), 
+    // represents the position that the box elevator should make way to yield for the ball elevator moving down to cargo low
+    CARGO_LOW_BELOW(Constants.BOX-Constants.SAFE_ELEVATOR_GAP), 
+    // represents the position that the ball elevator should make way to yield for the box elevator moving up to cargo low
     CARGO_LOW_ABOVE(Constants.BOX+Constants.SAFE_ELEVATOR_GAP), 
+    // represents the position that the box elevator should make way to yield for the ball elevator moving down to cargo high
     CARGO_HIGH_BELOW(Constants.BALL-Constants.SAFE_ELEVATOR_GAP),
     CARGO_HIGH(Constants.BALL),
+    // represents the position that the ball elevator should make way to yield for the box elevator moving up to cargo high
     CARGO_HIGH_ABOVE(Constants.BALL+Constants.SAFE_ELEVATOR_GAP);
 
     private final double val;
